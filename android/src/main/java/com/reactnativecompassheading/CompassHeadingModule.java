c24d866366774ef7174964265af12126deb204ed
Oli Barnett <oli.barnett@gmail.com>
2019-10-31 16:06:13 +0100

    Fix namespacing + typo

--- android/src/main/java/com/reactnativecompassheading/CompassHeadingModule.java
+++ android/src/main/java/com/reactnativecompassheading/CompassHeadingModule.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package com.reactnativecompassheading;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
--- android/src/main/java/com/reactnativecompassheading/CompassHeadingPackage.java
+++ android/src/main/java/com/reactnativecompassheading/CompassHeadingPackage.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package com.reactnativecompassheading;
 
 import java.util.Arrays;
 import java.util.Collections;
